79814651cf8d2bafa5b6df7b12cc82e66eec077d
JavaDevMatt <Matt@JavaDevMatt.pl>
2016-08-27 15:08:50 +0200

    flow control

---
+++ 001/flow-contol.py
@@ -0,0 +1,20 @@
+#--------------------
+#if test
+b = 'web'
+if b == True:
+    print('True')
+else:
+    print('False')
+
+
+#--------------------
+#while test
+i = 1
+
+while i < 10:
+    print(i)
+    i += 1
+    if(i == 6):
+        break
+
+print('Outside of while')
