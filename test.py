38a6da1b348c4e4ea12f5026f2885e50997bf889
JavaDevMatt <Matt@JavaDevMatt.pl>
2016-08-27 14:27:54 +0200

    python hello world

---
+++ test.py
@@ -0,0 +1,9 @@
+# Comment test
+
+print ("----------------------------")
+print ("Strting test script")
+print ("----------------------------")
+print ("")
+
+text = open("file.txt").read()
+print(text)
