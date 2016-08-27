756e6f0373255c19e908d136d3e23e78833eeeae
JavaDevMatt <Matt@JavaDevMatt.pl>
2016-08-27 16:01:05 +0200

    dicationary test

--- 001/dictionary.py
+++ 001/dictionary.py
@@ -5,5 +5,20 @@ myCat = {
     'age' : 1
     }
 
+print('-------------')
+
 print(myCat['size'])
 print(myCat['age'])
+print('-------------')
+
+for v in myCat.values():
+    print(v)
+print('-------------')
+
+for k in myCat.keys():
+    print(k)
+print('-------------')
+
+for i in myCat.items():
+    print(i)
+print('-------------')
