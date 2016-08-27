6344ebcb8a92da3d08ac1774cf1497e559421497
JavaDevMatt <Matt@JavaDevMatt.pl>
2016-08-27 15:36:19 +0200

    in and not in

--- 001/list-test.py
+++ 001/list-test.py
@@ -1,3 +1,6 @@
 supplies = ['pens', 'staplers', 'flame-throwers', 'binders']
 for i in range(len(supplies)):
     print('Index ' + str(i) + ' in supplies is: ' + supplies[i])
+
+print('binders' in supplies)
+print('binders' not in supplies)
