33246a986760c8d7334a54122ba55dbfbd65827a
JavaDevMatt <Matt@JavaDevMatt.pl>
2016-08-27 15:43:34 +0200

    goofing around

--- 001/list-test.py
+++ 001/list-test.py
@@ -4,3 +4,14 @@ for i in range(len(supplies)):
 
 print('binders' in supplies)
 print('binders' not in supplies)
+print(supplies.index('binders'))
+
+letters = ['1', '2', '3', '1', '1', '1', '1']
+print(letters)
+letters.remove('1')
+print(letters)
+
+while '1' in letters:
+    letters.remove('1')
+
+print(letters)
