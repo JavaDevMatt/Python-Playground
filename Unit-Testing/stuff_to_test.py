c67ef3d868f2b532684e1dc979c37526320e63c1
JavaDevMatt <Matt@JavaDevMatt.pl>
2016-08-28 13:17:32 +0200

    added test for regex

--- Unit-Testing/stuff_to_test.py
+++ Unit-Testing/stuff_to_test.py
@@ -3,3 +3,9 @@ def return_stuff(a):
 
 def add_numbers(a, b):
     return a + b
+
+import re
+
+def get_phone_numbers_list(s):
+    phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+    return phoneNumRegex.findall(s)
--- Unit-Testing/test1.py
+++ Unit-Testing/test1.py
@@ -25,3 +25,11 @@ def test_return_stuff_int_positive():
 
 def test_add_numbers():
     assert add_numbers(5, 15) == 20
+
+# regex 
+def test_phone_list_regex():
+    s = 'My number is 415-555-4242. And here is another one 123-123-1234.'
+    assert len(get_phone_numbers_list(s)) == 2
+    assert get_phone_numbers_list(s)[0] == '415-555-4242'
+    assert get_phone_numbers_list(s)[0] != 'bla'
+    
