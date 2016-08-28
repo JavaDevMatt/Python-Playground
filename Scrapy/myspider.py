98f397936c4040190f5e267eb2bc89e7921089ec
JavaDevMatt <Matt@JavaDevMatt.pl>
2016-08-28 16:25:06 +0200

    goofing with scrapy

---
+++ Scrapy/myspider.py
@@ -0,0 +1,16 @@
+import scrapy
+
+# usage
+# scrapy runspider myspider.py -o test.csv
+
+class BlogSpider(scrapy.Spider):
+    name = 'blogspider'
+    start_urls = ['https://blog.scrapinghub.com']
+
+    def parse(self, response):
+        for url in response.css('ul li a::attr("href")').re('.*/category/.*'):
+            yield scrapy.Request(response.urljoin(url), self.parse_titles)
+
+    def parse_titles(self, response):
+        for post_title in response.css('div.entries > ul > li a::text').extract():
+            yield {'title': post_title}
