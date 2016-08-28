68ab4a9a373e8c9d14f53c46e392c007df1b13d7
JavaDevMatt <Matt@JavaDevMatt.pl>
2016-08-28 10:29:50 +0200

    pyperclip password copy example

---
+++ 001/copy-pasta.py
@@ -0,0 +1,18 @@
+#An insecure copy pasta password locker program.
+
+import pyperclip, sys
+
+PASSWORDS = {'email': 'pass1',
+             'blog': 'pass2'}
+
+if len(sys.argv) < 2:
+    print('Usage: python3 copy-pasta.py [account] - copy account password')
+    sys.exit()
+
+account = sys.argv[1]
+
+if account in PASSWORDS:
+    pyperclip.copy(PASSWORDS[account])
+    print('Password for ' + account + ' copied to clipboard.')
+else:
+    print('There is no account named ' + account)
