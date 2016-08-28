75e42a33a5d900ab6be51568e8f9f241dc5ace84
JavaDevMatt <Matt@JavaDevMatt.pl>
2016-08-28 17:12:52 +0200

    dynamic data entry

--- SQLite/main.py
+++ SQLite/main.py
@@ -1,4 +1,7 @@
 import sqlite3
+import time
+import datetime
+import random
 
 conn = sqlite3.connect('tutorial.db')
 c = conn.cursor()
@@ -9,12 +12,31 @@ def create_table():
 
 
 def data_entry():
-    c.execute("INSERT INTO stuffToPlot VALUES(12123,'2014-01-11 13:53:39','Python2',8)")
+    c.execute("INSERT INTO stuffToPlot VALUES(1452549219,'2016-01-11 13:53:39','Python',6)")
 
     conn.commit()
     c.close()
     conn.close()
 
 
+def dynamic_data_entry():
+    unix = int(time.time())
+    date = str(datetime.datetime.fromtimestamp(unix).strftime('%Y-%m-%d %H:%M:%S'))
+    keyword = 'Python'
+    value = random.randrange(0, 10)
+
+    c.execute("INSERT INTO stuffToPlot (unix, datestamp, keyword, value) VALUES (?, ?, ?, ?)",
+              (unix, date, keyword, value))
+
+    conn.commit()
+
+
 create_table()
-data_entry()
+#data_entry()
+for i in range(10):
+    dynamic_data_entry()
+    time.sleep(1)
+
+
+c.close
+conn.close()
